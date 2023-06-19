e9782faa33817bdd32bbd658f5449ecf69d955b7
Your Name <you@example.com>
2023-06-19 10:55:41 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoHW3/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.PostmanEchoHW3;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldPostText(){
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Задача №3: Postman Echo") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+                .when()
+                .post("/post")
+        // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Задача №3: Postman Echo"));
+        ;
+    }
+}
